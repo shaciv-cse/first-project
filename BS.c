ea5a06107b3b9739366298e8867166f73812be6e
Shah Eqbal Shaciv <seqbal776@gmail.com>
2025-11-03 22:51:54 +0600

    Update BS.c

--- BS.c
+++ BS.c
@@ -1,23 +1,29 @@
 #include<stdio.h>
+void print(int arr[],int size);
+void babble(int arr[],int size);
 int main()
 {
-    int arr[]={11,44,33,77,32,47,42};
-    int n=sizeof(arr)/sizeof(arr[0]);
+    int arr[100],n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {scanf("%d",&arr[i]);
+    }
+    
     print(arr,n);
     babble(arr,n);
     print(arr,n);
     return 0;
 }
-int print(int arr[],int size)
+void print(int arr[],int size)
 {
     int i;
     for(i=0;i<size;i++)
     printf("%d  ",arr[i]);
     printf("\n");
 }
-int babble(int arr[],int n)
+void babble(int arr[],int size)
 {
-    int i,j;
+    int i,j,n;
     for(i=0;i<n-1;i++)
     {
         for(j=0;j<n-i-1;j++)
